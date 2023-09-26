e38cf52c89d551d7d1b11a4f2022865a3f065cad
Iam_Hey-k <60710389+harkeyhim@users.noreply.github.com>
2023-09-26 09:00:14 +0100

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Yusuf Akeem on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
